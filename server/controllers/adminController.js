4f82ff72062691a7e096e37df36730badc18149e
Nithin Ravi <nithinravi2bcommb@gmail.com>
2025-07-04 16:26:28 +0530

    stripe added

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -32,7 +32,7 @@ export const getDashboardData = async (req, res) => {
 // API TO GET ALL SHOWS
 export const getAllShows = async (req, res) => {
     try {
-        const shows = await Show.find({showDateTime: {$get: new Date()}}).populate('movie').sort({showDateTime: 1})
+        const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime: 1})
 
         res.json({success: true, shows})
     } catch (error) {
